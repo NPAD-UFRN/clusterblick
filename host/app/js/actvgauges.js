c001042d15c81ee435d234d1cbf1fa8441fd2a61
adelsondias <adelsondias@live.com>
2017-01-18 12:05:30 -0300

    ADD first app implementation: dfh gauges

---
+++ host/app/js/actvgauges.js
@@ -0,0 +1,49 @@
+var service0 = ssupervisor_dfh1[0];
+var lustre = ssupervisor_dfh1[3];
+
+var gauges = [];
+
+function createGauge(name, label, min, max)
+{
+	var config = 
+	{
+		size: 350,
+		label: label,
+		min: undefined != min ? min : 0,
+		max: undefined != max ? max : 100,
+		minorTicks: 5
+	}
+
+	var range = config.max - config.min;
+	config.yellowZones = [{ from: config.min + range*0.75, to: config.min + range*0.9 }];
+	config.redZones = [{ from: config.min + range*0.9, to: config.max }];
+
+	gauges[name] = new Gauge(name, config);
+	gauges[name].render();
+}
+
+function createGauges()
+{
+	createGauge("service0", "service0",0,Number(service0.Size.replace('T','').replace(',','.')));
+	createGauge("lustre", "Lustre",0,Number(lustre.Size.replace('T','').replace(',','.')));
+}
+
+function updateGauges()
+{
+	for (var key in gauges)
+	{
+		if (key=='service0'){
+			var value = Number(service0.Used.replace('T','').replace(',','.'));
+		}
+		if (key=='lustre'){
+			var value = Number(lustre.Used.replace('T','').replace(',','.'));
+		}
+		gauges[key].redraw(value);
+	}
+}
+
+function initialize()
+{
+	createGauges();
+	setInterval(updateGauges, 5000);
+}
--- host/main.py
+++ host/main.py
@@ -5,10 +5,15 @@ flag_ping,flag_down=0,0
 
 while True:
 	
-	config = c.readConfig('config.txt')
+	config = c.readConfig('config.txt','all')
 	data_dictionary = r.readRaw(config['raw_files'],config['raw_files_email'],config['path_raw'],config['path_data'])	
 	
-	#Sending email if something goes wrong
+	#service0 = data_dictionary['general_dictionary']['ssupervisor_dfh1'][0]
+	#lustre = data_dictionary['general_dictionary']['ssupervisor_dfh1'][-1]
+
+
+	#Sending email if something goes wrong - Ok
+	'''
 	downnodes = data_dictionary['email_dictionary'].get('ssupervisor_sinfo3',0)
 	pingbool = data_dictionary['email_dictionary'].get('ssupervisor_ping',None)
 	if pingbool==0 and flag_ping==0:
@@ -25,5 +30,6 @@ while True:
 		flag_down=1
 	elif downnodes<4 and flag_down==1:
 		flag_down=0
-		
+	'''	
+			
 	time.sleep(5)
